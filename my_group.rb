60c2a2977b5b34a09a268d5b3d5cd9df98f64923
Boa Matule <45321699+SnailCoder1@users.noreply.github.com>
2019-03-11 09:19:15 +0100

    Update my_group.rb

--- my_group.rb
+++ my_group.rb
@@ -7,14 +7,6 @@ person_3 = {name: "Mary", gender: "female",  age: 27}
 my_group = [{name: "Thomas", gender: " male", age: 45}, {name: "Noel", gender: "male", age: 34}, {name: "Mary", gender: "female",  age: 27}] 
 
 
-my_group.each do  |age, gender|
-    puts "is a  #{person.each[:age]} : #{person.each[:gender]}" 
-  end
-
-
-  my_group = [{name: "Thomas", gender: " male", age: 45}, {name: "Noel", gender: "male", age: 34}, {name: "Mary", gender: "female",  age: 27}] 
-
-
 my_group.each do  |my_group|
     puts "#{my_group[:name]} is a  #{my_group[:age]} old #{my_group[:gender]}." 
   end
