1eb264f57f3dfae1be4a6cac55cb4a4ed6f659f1
boa <boa.matule@gmail.com>
2019-03-11 08:53:46 +0100

    Solved some syntax error i had  missed in the ruby exercice

--- my_group.rb
+++ my_group.rb
@@ -4,14 +4,19 @@ person_1 = {name: "Thomas", gender: " male", age: 45}
 person_2 = {name: "Noel", gender: "male", age: 34}
 person_3 = {name: "Mary", gender: "female",  age: 27}
 
-my_group = ["person_1", "person_2", "person_3"] 
+my_group = [{name: "Thomas", gender: " male", age: 45}, {name: "Noel", gender: "male", age: 34}, {name: "Mary", gender: "female",  age: 27}] 
 
-
-
-my_group.age = ['Thomas', 'Noel', 'Mary']
 
 my_group.each do  |age, gender|
     puts "is a  #{person.each[:age]} : #{person.each[:gender]}" 
+  end
+
+
+  my_group = [{name: "Thomas", gender: " male", age: 45}, {name: "Noel", gender: "male", age: 34}, {name: "Mary", gender: "female",  age: 27}] 
+
+
+my_group.each do  |my_group|
+    puts "#{my_group[:name]} is a  #{my_group[:age]} old #{my_group[:gender]}." 
   end
 
 
